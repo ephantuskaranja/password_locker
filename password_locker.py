cb396ddf51d815a7cff2a5514839cc800d8bdb5e
Ephantus karanja <ephantusslogan@gmail.com>
2018-06-14 13:29:30 +0300

    separate logic flow,create controllers.py

--- password_locker.py
+++ password_locker.py
@@ -2,68 +2,67 @@ import secrets
 import string
 import getpass
 import pyperclip
+import controllers
+
+# def selector():
+#     '''
+#     function that controls the flow of the application
+#     '''
+#     print('please use the codes below to continue:')
+#     print("log-for registered users\n", "\nnew-for new user\n")
+#     selector_call = input()
+#     if selector_call == "new":
+#         userdata.create_user()
+#         # selector()
+#     elif selector_call == "log":
+#         login()
+#     else:
+#         print("illegal code input")
+#         selector()
 
 
-def selector():
-    '''
-    function that controls the flow of the application
-    '''
-    print('please use the codes below to continue:')
-    print("log-for registered users\n", "\nnew-for new user\n")
-    selector_call = input()
-    if selector_call == "new":
-        userdata.create_user()
-        # selector()
-    elif selector_call == "log":
-        login()
-    else:
-        print("illegal code input")
-        selector()
+# def access_controller():
+#     '''
+#     Gives options after login function is True
+#     '''
+#     print('genpass-for generate passwords or viewc-view saved credentials')
+#     access_call = input()
+#     if access_call == 'genpass':
+#         credentials.generate_password()
+#         print("*"*35)
+#         print('to continue, choose option')
+#         access_controller()
+#     elif access_call == 'viewc':
+#         credentials.show_generatedPass()
+#         print("copy credentials?:y or n")
+#         called = input().lower()
+#         if called == 'y':
+#             credentials.copy_credentials()
+
+#         else:
+#             print('thanks for your time.your are now exiting')
+#             print('*'*30)
+#             exit
+#     else:
+#         print('illegal input')
 
 
-def access_controller():
-    '''
-    Gives options after login function is True
-    '''
-    print('genpass-for generate passwords or viewc-view saved credentials')
-    access_call = input()
-    if access_call == 'genpass':
-        credentials.generate_password()
-        print("*"*35)
-        print('to continue, choose option')
-        access_controller()
-    elif access_call == 'viewc':
-        credentials.show_generatedPass()
-        print("copy credentials?:y or n")
-        called = input().lower()
-        if called == 'y':
-            credentials.copy_credentials()
-         
-        else:
-            print('thanks for your time.your are now exiting')
-            print('*'*30)
-            exit
-    else:
-        print('illegal input')
-
-
-def login():
-    '''
-    function that enables access to the system
-    '''
-    print('Logging in........')
-    username = input('Please enter username: ')
-    upass = getpass.getpass('Please enter password: ')
-    udata = {username: upass}
-    if str(udata) in open('login.txt').read():
-        # print("true")
-        print(f'correct credentials: Your are now logged in as {username}')
-        print('*'*30)
-        if True:
-            access_controller()
-    else:
-        print('incorrect credentials')
-
+# def login():
+#     '''
+#     function that enables access to the system
+#     '''
+#     print('Logging in........')
+#     username = input('Please enter username: ')
+#     upass = getpass.getpass('Please enter password: ')
+#     udata = {username: upass}
+#     if str(udata) in open('login.txt').read():
+#         # print("true")
+#         print(f'correct credentials: Your are now logged in as {username}')
+#         print('*'*30)
+#         if True:
+#             access_controller()
+#     else:
+#         print('incorrect credentials')
 
 
 class Userdata:
@@ -85,9 +84,9 @@ class Userdata:
         file.close()
         print(f"successfully created account for {username}")
         print('want to continue? y or n')
-        opt=input().lower()
-        if opt == 'y': 
-            login()
+        opt = input().lower()
+        if opt == 'y':
+            controllers.controllers.login()
         else:
             print('thanks for registering.you can login later')
             exit
@@ -110,12 +109,12 @@ class Credentials:
         alphabet = string.ascii_letters + string.digits
         password = ''.join(secrets.choice(alphabet) for i in range(20))
         file = open("password_keeper.txt", "a")
-        file.write("\n"+ accountFor+':'+password)
+        file.write("\n" + accountFor+':'+password)
         print(f"password for account {accountFor} generated")
         file.close()
     # generate_password()
 
-    def show_generatedPass(self):   
+    def show_generatedPass(self):
         inputted_username = input('Please enter username+accountFor:').lower()
         with open('password_keeper.txt') as f:
             for line in f:
@@ -124,18 +123,19 @@ class Credentials:
                     break
             else:
                 print('doesn\'t exists')
+                controllers.controllers.access_controller()
     # show_generatedPass()
-        
+
     def copy_credentials(self):
         '''
         function copies passwords for account to piperclip
         '''
         print("please specify account name to copy password:")
-        name=input()
-        f =  open ('password_keeper.txt', 'r')
+        name = input()
+        f = open('password_keeper.txt', 'r')
         for line in f.readlines():
             tag, key = line.strip().split(":")
-            if (name in tag) :
+            if (name in tag):
                 key = key.strip()
                 # print(key)
                 pyperclip.copy(key)
@@ -144,4 +144,4 @@ class Credentials:
 
     # copy_credentials()
 credentials = Credentials()
-selector()
+controllers.controllers.selector()
---
+++ test_passwordLocker.py
@@ -0,0 +1,10 @@
+import unittest
+
+import password_locker  # module to test
+
+
+class TestPassword_locker(unittest.TestCase):    #TestCase class
+    def test_add(self):
+        pass
+if __name__ == '__main__':
+    unittest.main() 
