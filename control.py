c37cf734ffbd047252ec9933d352ea720cc0bb4f
Ephantus karanja <ephantusslogan@gmail.com>
2018-06-22 13:06:48 +0300

    combine controllers and passwordlocker_py files to control.py

--- control.py
+++ control.py
@@ -3,6 +3,9 @@ import string
 import getpass
 import pyperclip
 import controllers
+import pyperclip
+
+
 
 
 class Userdata:
@@ -26,10 +29,10 @@ class Userdata:
         print('want to continue? y or n')
         opt = input().lower()
         if opt == 'y':
-            controllers.controllers.login()
-        else:
+            controllers.login()
+        elif opt== 'n':
             print('thanks for registering.you can login later')
-            exit
+        exit()
 
 
 userdata = Userdata()
@@ -64,7 +67,7 @@ class Credentials:
                     break
             else:
                 print('doesn\'t exists')
-                controllers.controllers.access_controller()
+                controllers.access_controller()
     # show_generatedPass()
 
     def copy_credentials(self):
@@ -82,7 +85,72 @@ class Credentials:
                 pyperclip.copy(key)
                 print(f"password for account {name} copied")
         print('your now exiting...')        
-        exit
+        exit()
     # copy_credentials()
 
 credentials = Credentials()
+
+
+
+class Controllers():
+    def selector(self):
+        '''
+        function that controls the flow of the application
+        '''
+        print('please use the codes below to continue:')
+        print("log-for registered users\n", "\nnew-for new user\n")
+        selector_call = str(input())
+        if selector_call == "new":
+           userdata.create_user()
+            # selector()
+        elif selector_call == "log":
+            controllers.login()
+        else:
+            print("illegal code input")
+            controllers.selector()
+
+
+    def access_controller(self):
+        '''
+        Gives options after login function is True
+        '''
+        print('genpass-for generate passwords or viewc-view saved credentials')
+        access_call = str(input())
+        if access_call == 'genpass':
+            credentials.generate_password()
+            print("*"*35)
+            print('to continue, choose option')
+            controllers.access_controller()
+        elif access_call == 'viewc':
+            credentials.show_generatedPass()
+            print("copy credentials?:y or n")
+            called = input().lower()
+            if called == 'y':
+               credentials.copy_credentials()
+
+            else:
+                print('thanks for your time.your are now exiting')
+                print('*'*30)
+                exit
+        else:
+            print('illegal input')
+
+
+    def login(self):
+        '''
+        function that enables access to the system
+        '''
+        print('Logging in........')
+        username = str(input('Please enter username: '))
+        upass = getpass.getpass('Please enter password: ')
+        udata = {username: upass}
+        if str(udata) in open('login.txt').read():
+            # print("true")
+            print(f'correct credentials: Your are now logged in as {username}')
+            print('*'*30)
+            if True:
+              controllers.access_controller()
+        else:
+            print('incorrect credentials')
+
+controllers = Controllers()
